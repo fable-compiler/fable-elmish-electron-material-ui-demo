9f7899870373e27901528f5deb99cf0e0bedb96c
Christer van der Meeren <cmeeren@protonmail.com>
2019-08-31 13:08:29 +0200

    Add colors

---
+++ src/Feliz.MaterialUI/Colors.fs
@@ -0,0 +1,23 @@
+﻿module Feliz.MaterialUI.Colors
+
+open Fable.Core.JsInterop
+
+let red : Color = importDefault "@material-ui/core/colors/red"
+let pink : Color = importDefault "@material-ui/core/colors/pink"
+let purple : Color = importDefault "@material-ui/core/colors/purple"
+let deepPurple : Color = importDefault "@material-ui/core/colors/deepPurple"
+let indigo : Color = importDefault "@material-ui/core/colors/indigo"
+let blue : Color = importDefault "@material-ui/core/colors/blue"
+let lightBlue : Color = importDefault "@material-ui/core/colors/lightBlue"
+let cyan : Color = importDefault "@material-ui/core/colors/cyan"
+let teal : Color = importDefault "@material-ui/core/colors/teal"
+let green : Color = importDefault "@material-ui/core/colors/green"
+let lightGreen : Color = importDefault "@material-ui/core/colors/lightGreen"
+let lime : Color = importDefault "@material-ui/core/colors/lime"
+let yellow : Color = importDefault "@material-ui/core/colors/yellow"
+let amber : Color = importDefault "@material-ui/core/colors/amber"
+let orange : Color = importDefault "@material-ui/core/colors/orange"
+let deepOrange : Color = importDefault "@material-ui/core/colors/deepOrange"
+let brown : Color = importDefault "@material-ui/core/colors/brown"
+let grey : Color = importDefault "@material-ui/core/colors/grey"
+let blueGrey : Color = importDefault "@material-ui/core/colors/blueGrey"

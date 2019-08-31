f40a06d2e2d1479f5892389f3cf00f5873949fda
Christer van der Meeren <cmeeren@protonmail.com>
2019-08-31 17:41:54 +0200

    Add useMediaQuery

---
+++ src/Feliz.MaterialUI/Hooks.fs
@@ -0,0 +1,23 @@
+﻿namespace Feliz.MaterialUI
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+
+module internal HookImports =
+
+  let useMediaQuery (query: string) : bool =
+    importDefault "@material-ui/core/useMediaQuery"
+
+  let useMediaQuery_theme (getQuery: Theme -> string) : bool =
+    importDefault "@material-ui/core/useMediaQuery"
+
+
+[<Erase>]
+type Hooks =
+
+  static member inline useMediaQuery (query: string) : bool =
+    HookImports.useMediaQuery query
+
+  static member inline useMediaQuery (getQuery: Theme -> string) : bool =
+    HookImports.useMediaQuery_theme getQuery
